b8f40317889dd9ac41239d2a6725c22e7fabe66e
hamdi <javs_shao@163.com>
2019-02-08 13:44:19 +0800

    Model_用户

--- ebusiness/goods/models.py
+++ ebusiness/goods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# 用户
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
