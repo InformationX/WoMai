7e1b22230778302bf07c4102bbbadc5e4c0c11d3
hamdi <javs_shao@163.com>
2019-02-08 13:47:55 +0800

    Model_模型类编写

--- ebusiness/goods/models.py
+++ ebusiness/goods/models.py
@@ -20,3 +20,31 @@ class Goods(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# 收货地址
+class Address(models.Model):
+    user = models.ForeignKey(User)  # 关联用户id
+    address = models.CharField(max_length=50)  # 地址
+    phone = models.CharField(max_length=15)  # 电话
+
+    def __str__(self):
+        return self.address
+
+
+# 总订单
+class Orders(models.Model):
+    address = models.ForeignKey(Address)  # 关联送货地址id
+    create_time = models.DateTimeField(auto_now=True)  # 创建时间
+    status = models.BooleanField()  # 订单状态
+
+    def __str__(self):
+        return self.create_time
+
+
+# 一个订单
+class Order(models.Model):
+    order = models.ForeignKey(Orders)  # 关联总订单id
+    user = models.ForeignKey(User)  # 关联用户id
+    goods = models.ForeignKey(Goods)  # 关联商品id
+    count = models.IntegerField()  # 数量
